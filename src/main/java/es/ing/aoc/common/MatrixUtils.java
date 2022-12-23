2d8589a43b587bfbc1f01c28f133d786c794acdd
Manuel Fernández Molina <manuel.fernandez.molina@ing.com>
2022-12-23 18:19:25 +0100

    feat: d22 part2 partial cubes reshaped

--- src/main/java/es/ing/aoc/common/MatrixUtils.java
+++ src/main/java/es/ing/aoc/common/MatrixUtils.java
@@ -8,26 +8,66 @@ public class MatrixUtils {
         throw new RuntimeException("Constructor not meant to be called");
     }
 
-    public static void rotateRightAndMove(int[][] matrix, Point offset){
-        int[][] result = new int[matrix.length][matrix.length];
-        for (int i=0; i< matrix.length; i++){
-            for (int j=0; j< matrix.length; j++){
-                result[j][matrix.length-i] = matrix[i][j];
+    @SuppressWarnings("unchecked")
+    public static <T> void rotateRight(Class<T> type, T[][] matrix, Point offset, int size) {
+        T[][] result = (T[][]) Array.newInstance(type, size, size);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                result[j][size - i - 1] = matrix[i + offset.x][j + offset.y];
             }
         }
-
-        copy(matrix, result, offset);
+        copy(result, matrix, offset);
     }
 
-    public static void copy(int[][] origin, int[][] destination){
-        copy(origin, destination, new Point(0, 0));
-    }
-
-    public static void copy(int[][] origin, int[][] destination, Point offset){
-        for (int i=0; i< origin.length; i++) {
-            for (int j = 0; j < origin.length; j++) {
-                destination[i+ offset.x][j+ offset.y] = origin[i][j];
+    @SuppressWarnings("unchecked")
+    public static <T> void rotateLeft(Class<T> type, T[][] matrix, Point offset, int size) {
+        T[][] result = (T[][]) Array.newInstance(type, size, size);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                result[i][j] = matrix[j + offset.x][size - i - 1 + offset.y];
             }
         }
+        copy(result, matrix, offset);
+    }
+
+    public static <T> void copy(T[][] origin, T[][] destination, Point offset) {
+        for (int i = 0; i < origin.length; i++) {
+            System.arraycopy(origin[i], 0, destination[i + offset.x], offset.y, origin.length);
+        }
+    }
+
+    public static <T> void moveSubMatrix(T[][] matrix, Point origin, Point destination, int size, T clearCell){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i+ destination.x][j + destination.y] = matrix[i + origin.x][j + origin.y];
+                matrix[i + origin.x][j + origin.y] = clearCell;
+            }
+        }
+    }
+
+    public static <T> void printMatrix(T[][] matrix) {
+        System.out.println("-------------------------------");
+        for (T[] row : matrix) {
+            for (T cell : row) {
+                System.out.printf("%-3s", cell);
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------------");
+    }
+
+    public static void main(String[] args) {
+
+        Integer[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}};
+
+        printMatrix(matrix);
+        rotateRight(Integer.class, matrix, new Point(0, 0), 2);
+        printMatrix(matrix);
+        moveSubMatrix(matrix, new Point(0, 0), new Point(2, 2), 2, 0);
+        printMatrix(matrix);
     }
 }
--- src/main/java/es/ing/aoc/y2022/Day22.java
+++ src/main/java/es/ing/aoc/y2022/Day22.java
@@ -1,18 +1,16 @@
 package es.ing.aoc.y2022;
 
-import static es.ing.aoc.y2022.Day22.Direction.RIGHT;
 import static es.ing.aoc.y2022.Day22.Tile.FLOOR;
 import static es.ing.aoc.y2022.Day22.Tile.VOID;
 import static es.ing.aoc.y2022.Day22.Tile.WALL;
 
 import es.ing.aoc.common.Day;
+import es.ing.aoc.common.MatrixUtils;
 import es.ing.aoc.common.Pair;
 import es.ing.aoc.common.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.UnaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -75,43 +73,55 @@ public class Day22 extends Day {
     }
 
     public enum Turn {
-        RIGHT(Direction::turnRight),
-        LEFT(Direction::turnLeft),
-        REVERSE(Direction::turnRight, Direction::turnRight),
-        NONE();
+        RIGHT,
+        LEFT,
+        REVERSE,
+        NONE;
+    }
 
-        private final UnaryOperator<Direction>[] dirs;
+    static class Cell {
+        Tile tile;
+        Point originPosition;
 
-        Turn(UnaryOperator<Direction>... dirs) {
-            this.dirs = dirs;
+        public Cell(Tile tile, Point originPosition) {
+            this.tile = tile;
+            this.originPosition = originPosition;
         }
 
-        public Direction getNewDirection(Direction current) {
-            Direction next = current;
-            for (UnaryOperator<Direction> op : dirs) {
-                next = op.apply(next);
-            }
-            return next;
+        public static Cell of(Tile tile, Point originPosition) {
+            return new Cell(tile, originPosition);
         }
     }
 
     @Override
     protected String part1(String fileContents) throws Exception {
-        Tile[][] board = readBoardFromFile(fileContents);
+        Cell[][] board = readBoardFromFile(fileContents);
         List<String> instructions = getInstructions(fileContents);
-        Pair<Point, Direction> start = Pair.of(getStartingPoint(board), RIGHT);
+        Pair<Point, Direction> start = Pair.of(getStartingPoint(board), Direction.RIGHT);
         Pair<Point, Direction> end = followPath(board, instructions, start);
-        return String.valueOf(((end.a.x + 1) * 1000) + ((end.a.y + 1) * 4) + end.b.points);
+        return String.valueOf(getPositionPoints(end));
     }
 
     @Override
     protected String part2(String fileContents) throws Exception {
-        Tile[][] board = readBoardFromFile(fileContents);
-        createCubeFromBoard(board, 4);
+        Cell[][] board = readBoardFromFile(fileContents);
+        transformBoardToStandardCube(board, inferSideSizeFromBoard(board));
         return String.valueOf(2);
     }
 
-    private Pair<Point, Direction> followPath(Tile[][] board, List<String> instructions, Pair<Point, Direction> start) {
+    private int getPositionPoints(Pair<Point, Direction> position) {
+        return ((position.a.x + 1) * 1000) + ((position.a.y + 1) * 4) + position.b.points;
+    }
+
+    private int inferSideSizeFromBoard(Cell[][] board) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // Input cube (for good shaped ones) will be always 3x4 or 4x3, so:
+        return Math.max(rows, cols) - Math.min(rows, cols);
+    }
+
+    private Pair<Point, Direction> followPath(Cell[][] board, List<String> instructions, Pair<Point, Direction> start) {
 
         Pair<Point, Direction> current = start;
 
@@ -125,12 +135,12 @@ public class Day22 extends Day {
 
                 for (int i = 0; i < pathLength; i++) {
                     Pair<Point, Direction> newPos = moveToSafePosition(current, board);
-                    while (VOID.equals(board[newPos.a.x][newPos.a.y])) {
+                    while (VOID.equals(board[newPos.a.x][newPos.a.y].tile)) {
                         newPos = moveToSafePosition(newPos, board);
                     }
-                    if (FLOOR.equals(board[newPos.a.x][newPos.a.y])) {
+                    if (FLOOR.equals(board[newPos.a.x][newPos.a.y].tile)) {
                         current = newPos;
-                    } else if (WALL.equals(board[newPos.a.x][newPos.a.y])) {
+                    } else if (WALL.equals(board[newPos.a.x][newPos.a.y].tile)) {
                         break;
                     }
                 }
@@ -139,7 +149,7 @@ public class Day22 extends Day {
         return current;
     }
 
-    private Pair<Point, Direction> moveToSafePosition(Pair<Point, Direction> current, Tile[][] board) {
+    private Pair<Point, Direction> moveToSafePosition(Pair<Point, Direction> current, Cell[][] board) {
         Point newPos = current.b.fn.apply(current.a);
         return Pair.of(new Point((newPos.x + board.length) % board.length, (newPos.y + board[0].length) % board[0].length), current.b);
     }
@@ -156,60 +166,165 @@ public class Day22 extends Day {
         return steps;
     }
 
-    private Tile[][] readBoardFromFile(String fileContents) {
+    private Cell[][] readBoardFromFile(String fileContents) {
         List<String> allLines = new ArrayList<>(Arrays.asList(fileContents.split(System.lineSeparator()))); // when input file is multiline
 
         allLines.remove(allLines.size() - 1);
         allLines.remove(allLines.size() - 1);
 
-        Tile[][] board = new Tile[allLines.size()][allLines.stream().mapToInt(String::length).max().orElse(0)];
+        int maxColumns = allLines.stream().mapToInt(String::length).max().orElse(0);
+        Cell[][] board = new Cell[allLines.size()][maxColumns];
 
         String line;
         for (int i = 0; i < allLines.size(); i++) {
-            Arrays.fill(board[i], VOID);
             line = allLines.get(i);
-            for (int j = 0; j < line.length(); j++) {
-                board[i][j] = Tile.of(String.valueOf(line.charAt(j)));
+            for (int j = 0; j < maxColumns; j++) {
+                if (j < line.length()) {
+                    board[i][j] = Cell.of(Tile.of(String.valueOf(line.charAt(j))), Point.of(i, j));
+                } else {
+                    board[i][j] = Cell.of(Tile.VOID, Point.of(i, j));
+                }
             }
         }
         return board;
     }
 
-    private void printBoard(Tile[][] board) {
+    private void printBoard(Cell[][] board) {
         System.out.println("-------------------------------------------");
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j]);
+        for (Cell[] tiles : board) {
+            for (Cell tile : tiles) {
+                System.out.print(tile);
             }
             System.out.println();
         }
         System.out.println("-------------------------------------------");
     }
 
-    private void createCubeFromBoard(Tile[][] board, int dim) {
-        List<Point> sides = new ArrayList<>();
+    private void transformBoardToStandardCube(Cell[][] board, int dim) {
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (!VOID.equals(board[i][j]) && i % dim == 0 && j % dim == 0) {
-                    sides.add(new Point(i / dim, j / dim));
+        boolean shapedCube = false;
+        do {
+            List<Point> sides = calculateActiveSidesFromBoard(board, dim);
+            System.out.println("Current sides: " + sides);
+            printReadableSides(sides);
+
+            int baseCoord = 1;
+            int horizontalCubes = (int) sides.stream().filter(p -> p.x == baseCoord).count();
+            int verticalCubes = (int) sides.stream().filter(p -> p.y == baseCoord).count();
+
+            if (horizontalCubes == 4) {
+
+                Point topCube = sides.stream().filter(p -> p.x == baseCoord - 1).findFirst().orElseThrow(() -> new RuntimeException("Wrong cube!"));
+                Point downCube = sides.stream().filter(p -> p.x == baseCoord + 1).findFirst().orElseThrow(() -> new RuntimeException("Wrong cube!"));
+
+                if (topCube.y > baseCoord) {
+                    rotateAndMoveSide(board, topCube, dim, Turn.LEFT, Direction.LEFT);
+                } else if (topCube.y < baseCoord) {
+                    rotateAndMoveSide(board, topCube, dim, Turn.RIGHT, Direction.RIGHT);
+                } else {
+                    System.out.println("Top cube is OK!");
                 }
+                if (downCube.y > baseCoord) {
+                    rotateAndMoveSide(board, downCube, dim, Turn.RIGHT, Direction.LEFT);
+                } else if (downCube.y < baseCoord) {
+                    rotateAndMoveSide(board, downCube, dim, Turn.LEFT, Direction.RIGHT);
+                } else {
+                    System.out.println("Down cube is OK!");
+                }
+                shapedCube = topCube.y == baseCoord && downCube.y == baseCoord;
+
+            } else if (verticalCubes == 4) {
+
+                Point leftCube = sides.stream().filter(p -> p.y == baseCoord - 1).findFirst().orElseThrow(() -> new RuntimeException("Wrong cube!"));
+                Point rightCube = sides.stream().filter(p -> p.y == baseCoord + 1).findFirst().orElseThrow(() -> new RuntimeException("Wrong cube!"));
+
+                if (leftCube.x > baseCoord) {
+                    rotateAndMoveSide(board, leftCube, dim, Turn.RIGHT, Direction.UP);
+                } else if (leftCube.x < baseCoord) {
+                    rotateAndMoveSide(board, leftCube, dim, Turn.LEFT, Direction.DOWN);
+                } else {
+                    System.out.println("Left cube is OK!");
+                }
+                if (rightCube.x > baseCoord) {
+                    rotateAndMoveSide(board, rightCube, dim, Turn.LEFT, Direction.UP);
+                } else if (rightCube.x < baseCoord) {
+                    rotateAndMoveSide(board, rightCube, dim, Turn.RIGHT, Direction.DOWN);
+                } else {
+                    System.out.println("Right cube is OK!");
+                }
+                shapedCube = leftCube.x == baseCoord && rightCube.x == baseCoord;
+
+            } else if (horizontalCubes == 3) {
+                int leftCol = sides.stream().filter(p -> p.x == baseCoord).mapToInt(p -> p.y).min().orElse(-1);
+                int rightCol = sides.stream().filter(p -> p.x == baseCoord).mapToInt(p -> p.y).max().orElse(-1);
+
+                List<Point> cornerSpots = List.of(
+                        Point.of(baseCoord - 1, leftCol - 1),
+                        Point.of(baseCoord + 1, leftCol - 1),
+                        Point.of(baseCoord - 1, rightCol + 1),
+                        Point.of(baseCoord + 1, rightCol + 1));
+
+                Point firstCorner = null;
+                for (Point corner : cornerSpots) {
+                    if (sides.contains(corner)) {
+                        firstCorner = corner;
+                        break;
+                    }
+                }
+                if (firstCorner == null) {
+                    throw new RuntimeException("No first corner found in sides array!");
+                }
+                System.out.println("First corner(relative): " + firstCorner);
+
+                if (firstCorner.x > baseCoord && firstCorner.y > rightCol) {
+                    rotateAndMoveSide(board, firstCorner, dim, Turn.LEFT, Direction.UP);
+                } else if (firstCorner.x > baseCoord && firstCorner.y < leftCol) {
+                    rotateAndMoveSide(board, firstCorner, dim, Turn.RIGHT, Direction.UP);
+                } else if (firstCorner.x < baseCoord && firstCorner.y > rightCol) {
+                    rotateAndMoveSide(board, firstCorner, dim, Turn.RIGHT, Direction.DOWN);
+                } else {
+                    rotateAndMoveSide(board, firstCorner, dim, Turn.LEFT, Direction.DOWN);
+                }
+            } else if (verticalCubes == 3) {
+                int topRow = sides.stream().filter(p -> p.y == baseCoord).mapToInt(p -> p.x).min().orElse(-1);
+                int downRow = sides.stream().filter(p -> p.y == baseCoord).mapToInt(p -> p.x).max().orElse(-1);
+
+                List<Point> cornerSpots = List.of(
+                        Point.of(topRow - 1, baseCoord - 1),
+                        Point.of(topRow - 1, baseCoord + 1),
+                        Point.of(downRow + 1, baseCoord - 1),
+                        Point.of(downRow + 1, baseCoord + 1));
+
+                Point firstCorner = null;
+                for (Point corner : cornerSpots) {
+                    if (sides.contains(corner)) {
+                        firstCorner = corner;
+                        break;
+                    }
+                }
+                if (firstCorner == null) {
+                    throw new RuntimeException("No first corner found in sides array!");
+                }
+                System.out.println("First corner(relative): " + firstCorner);
+
+                if (firstCorner.y > baseCoord && firstCorner.x > downRow) {
+                    rotateAndMoveSide(board, firstCorner, dim, Turn.RIGHT, Direction.LEFT);
+                } else if (firstCorner.y > baseCoord && firstCorner.x < topRow) {
+                    rotateAndMoveSide(board, firstCorner, dim, Turn.LEFT, Direction.LEFT);
+                } else if (firstCorner.y < baseCoord && firstCorner.x > downRow) {
+                    rotateAndMoveSide(board, firstCorner, dim, Turn.LEFT, Direction.RIGHT);
+                } else {
+                    rotateAndMoveSide(board, firstCorner, dim, Turn.RIGHT, Direction.RIGHT);
+                }
+
+            } else {
+                throw new RuntimeException("No base condition detected. Is input cube a correct shaped cube?");
             }
-        }
 
-        System.out.println(sides);
+        } while (!shapedCube);
 
-
-        int verticalCubes = (int) sides.stream().filter(p -> p.y == 1).count();
-        int horizontalCubes = (int) sides.stream().filter(p -> p.x == 1).count();
-
-
-
-
-
-
-
+/*
         Map<Point, List<Pair<Direction, Point>>> sideNeighbours = new HashMap<>();
         sides.forEach(s -> sideNeighbours.put(s, new ArrayList<>()));
 
@@ -225,11 +340,82 @@ public class Day22 extends Day {
         }
 
         System.out.println(sideNeighbours);
+
+ */
     }
 
-    private Point getStartingPoint(Tile[][] board) {
+    private void rotateAndMoveSide(Cell[][] board, Point side, int dim, Turn turn, Direction movement) {
+        System.out.printf(" >>>>> Rotate & Move command: %s - %s - %s\n", side, turn, movement);
+        Point realCoords = new Point(side.x * dim, side.y * dim);
+
+        switch (turn) {
+            case LEFT:
+                MatrixUtils.rotateLeft(Cell.class, board, realCoords, dim);
+                break;
+            case RIGHT:
+                MatrixUtils.rotateRight(Cell.class, board, realCoords, dim);
+                break;
+            case REVERSE:
+                MatrixUtils.rotateLeft(Cell.class, board, realCoords, dim);
+                MatrixUtils.rotateLeft(Cell.class, board, realCoords, dim);
+                break;
+            case NONE:
+                System.out.println("No rotation needed!");
+        }
+
+        switch (movement) {
+            case UP:
+                MatrixUtils.moveSubMatrix(board, realCoords, new Point(realCoords.x - dim, realCoords.y), dim, createNewVoid());
+                break;
+            case DOWN:
+                MatrixUtils.moveSubMatrix(board, realCoords, new Point(realCoords.x + dim, realCoords.y), dim, createNewVoid());
+                break;
+            case LEFT:
+                MatrixUtils.moveSubMatrix(board, realCoords, new Point(realCoords.x, realCoords.y - dim), dim, createNewVoid());
+                break;
+            case RIGHT:
+                MatrixUtils.moveSubMatrix(board, realCoords, new Point(realCoords.x, realCoords.y + dim), dim, createNewVoid());
+                break;
+        }
+    }
+
+    private List<Point> calculateActiveSidesFromBoard(Cell[][] board, int dim) {
+        List<Point> sides = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (!VOID.equals(board[i][j].tile) && i % dim == 0 && j % dim == 0) {
+                    sides.add(new Point(i / dim, j / dim));
+                }
+            }
+        }
+        return sides;
+    }
+
+    private Cell createNewVoid() {
+        return Cell.of(VOID, new Point(-1, -1));
+    }
+
+    private void printReadableSides(List<Point> sides) {
+        int n = 1;
+        System.out.printf("    %d   %d   %d   %d\n", 0, 1, 2, 3);
+        System.out.print("  o---o---o---o---o\n");
+        for (int x = 0; x < 4; x++) {
+            System.out.printf("%d |", x);
+            for (int y = 0; y < 4; y++) {
+                if (sides.contains(new Point(x, y))) {
+                    System.out.printf(" %d |", n++);
+                } else {
+                    System.out.print("   |");
+                }
+            }
+            System.out.print("\n  o---o---o---o---o\n");
+        }
+    }
+
+    private Point getStartingPoint(Cell[][] board) {
         for (int j = 0; j < board[0].length; j++) {
-            if (FLOOR.equals(board[0][j])) {
+            if (FLOOR.equals(board[0][j].tile)) {
                 return new Point(0, j);
             }
         }
@@ -238,6 +424,6 @@ public class Day22 extends Day {
     }
 
     public static void main(String[] args) {
-        Day.run(Day22::new, "2022/D22_small.txt");//, "2022/D22_full.txt");
+        Day.run(Day22::new, "2022/D22_small.txt", "2022/D22_full.txt");
     }
 }
