2f10783a8fdbd6d1a1b261821cf573f8da96ee7c
Manuel Fernández Molina <manuel.fernandez.molina@ing.com>
2023-12-02 07:33:53 +0100

    feat: 2023 D3 base files

---
+++ src/test/java/es/ing/aoc/y2023/Day3Test.java
@@ -0,0 +1,24 @@
+package es.ing.aoc.y2023;
+
+import es.ing.aoc.common.Day;
+import es.ing.aoc.common.Pair;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day3Test {
+
+  @Test
+  void testSmallProblem() {
+    Pair<String, String> results = Day.run(Day3::new, "2023/D3_small.txt");
+    assertEquals("", results.a);
+    assertEquals("", results.b);
+  }
+
+  @Test
+  void testFullProblem() {
+    Pair<String, String> results = Day.run(Day3::new, "2023/D3_full.txt");
+    assertEquals("", results.a);
+    assertEquals("", results.b);
+  }
+}
