ea9d8338b6bc8a4f9518173f25488232b8f8ef82
Manuel Fernández Molina <manuel.fernandez.molina@ing.com>
2023-12-12 14:48:02 +0100

    refactor: 2023 D12 backtracking with cache (not working)

--- src/main/java/es/ing/aoc/y2023/Day12.java
+++ src/main/java/es/ing/aoc/y2023/Day12.java
@@ -1,15 +1,21 @@
 package es.ing.aoc.y2023;
 
 import es.ing.aoc.common.Day;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import static es.ing.aoc.y2023.Day12.Record.OPERATIONAL;
+import static es.ing.aoc.y2023.Day12.Record.UNKNOWN;
 
 public class Day12 extends Day {
 
@@ -24,103 +30,137 @@ public class Day12 extends Day {
       this.value = value;
     }
 
-    public String getValue() {
-      return String.valueOf(value);
-    }
-
     public static Record of(char c) {
       return Arrays.stream(Record.values()).filter(r -> r.value==c).findFirst().orElseThrow(() -> new RuntimeException("Record not found!"));
+    }
+
+    @Override
+    public String toString() {
+      return String.valueOf(value);
     }
   }
 
   @Override
   protected String part1(String fileContents) throws Exception {
+    //return algorithm(fileContents, 1);
+    return "";
+  }
 
+  @Override
+  protected String part2(String fileContents) throws Exception {
+    return algorithm(fileContents, 5);
+  }
+
+  private String algorithm(String fileContents, int folding) {
     String[] lines = fileContents.split(System.lineSeparator());
     List<Pair<List<Record>, List<Integer>>> info = new ArrayList<>();
 
     for (String line : lines) {
       String[] parts = line.split(" ");
-      info.add(Pair.of(
-          parts[0].chars().mapToObj(c -> Record.of((char) c)).toList(),
-          Arrays.stream(parts[1].split(",")).mapToInt(Integer::parseInt).boxed().toList()
-      ));
+      info.add(
+          fillRecords(
+              Pair.of(
+                  parts[0].chars().mapToObj(c -> Record.of((char) c)).toList(),
+                  Arrays.stream(parts[1].split(",")).mapToInt(Integer::parseInt).boxed().toList()),
+              folding));
     }
 
-    int counter = 0, i=1;
+    int counter = 0, i = 0;
     for (var comb : info) {
       Pattern pattern = buildPattern(comb.getRight());
-      counter += calculateCombinations(comb, pattern, new HashSet<>());
-      System.out.printf("%-5d - %d", i++, counter);
+      counter += calculateCombinations(comb, pattern, new HashMap<>());
+      System.out.printf("%d  - %d\n", i++, counter);
     }
 
     return String.valueOf(counter);
   }
 
-  @Override
-  protected String part2(String fileContents) throws Exception {
-
-    String[] lines = fileContents.split(System.lineSeparator());
-
-
-    return String.valueOf("");
-  }
-
-  private int calculateCombinations(Pair<List<Record>, List<Integer>> info, Pattern numbersPattern, Set<String> valids) {
-    if (info.getLeft().contains(Record.UNKNOWN)) {
-      List<Record> newRecord = new ArrayList<>();
-      for (int i = 0; i < info.getLeft().size(); i++) {
-        if (Record.UNKNOWN.equals(info.getLeft().get(i))) {
-
-          List<Record> whenOperational = new ArrayList<>(newRecord);
-          whenOperational.add(Record.OPERATIONAL);
-          if (i < info.getLeft().size() - 1) {
-            whenOperational.addAll(info.getLeft().subList(i + 1, info.getLeft().size()));
-          }
-          calculateCombinations(Pair.of(whenOperational, info.getRight()), numbersPattern, valids);
-
-          List<Record> whenDamaged = new ArrayList<>(newRecord);
-          whenDamaged.add(Record.DAMAGED);
-          if (i < info.getLeft().size() - 1) {
-            whenDamaged.addAll(info.getLeft().subList(i + 1, info.getLeft().size()));
-          }
-          calculateCombinations(Pair.of(whenDamaged, info.getRight()), numbersPattern, valids);
-
-        }
-        newRecord.add(info.getLeft().get(i));
+  private Pair<List<Record>, List<Integer>> fillRecords(Pair<List<Record>, List<Integer>> info, int n) {
+    List<Record> newRecords = new ArrayList<>();
+    List<Integer> newNumbers = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      newRecords.addAll(info.getLeft());
+      newNumbers.addAll(info.getRight());
+      if (i < n - 1) {
+        newRecords.add(UNKNOWN);
       }
-    } else {
-      isCombinationValid(info, numbersPattern, valids);
     }
 
-    return valids.size();
+    return Pair.of(newRecords, newNumbers);
   }
 
-  private boolean isCombinationValid(Pair<List<Record>, List<Integer>> info, Pattern numbersPattern, Set<String> valids) {
-    String strToMatch = info.getLeft().stream().map(Record::getValue).collect(Collectors.joining(""));
-    boolean result = numbersPattern.matcher(strToMatch).matches();
-    //System.out.printf("%-20s%s\n", strToMatch, result);
+  private int calculateCombinations(Pair<List<Record>, List<Integer>> info, Pattern numbersPattern, Map<Triple<Integer, Integer, Record>, Integer> cache) {
 
-    if (result) {
-      valids.add(strToMatch);
+    String strToMatch = info.getLeft().stream().map(Record::toString).collect(Collectors.joining(StringUtils.EMPTY));
+    Matcher matcher = numbersPattern.matcher(strToMatch);
+    System.out.println("before");
+    boolean matchResult = matcher.matches();
+    System.out.println("after");
+    boolean allResolved = !info.getLeft().contains(UNKNOWN);
+    int firstUnknown = info.getLeft().indexOf(UNKNOWN);
+
+    if (matchResult) {
+      int groups = 0;
+
+      for (int i = 1; i <= matcher.groupCount(); i++) {
+        if (!matcher.group(i).contains(UNKNOWN.toString())) {
+          groups++;
+        }
+      }
+
+      if (allResolved || groups==info.getRight().size()) {
+        return 1;
+      } else {
+
+        Triple<Integer, Integer, Record> cacheKey = Triple.of(groups, firstUnknown, firstUnknown > 0 ? info.getLeft().get(firstUnknown - 1):UNKNOWN);
+
+        if (!cache.containsKey(cacheKey)) {
+          List<Record> newRecord = new ArrayList<>();
+          for (int i = 0; i < info.getLeft().size(); i++) {
+            if (UNKNOWN.equals(info.getLeft().get(i))) {
+
+              List<Record> whenOperational = new ArrayList<>(newRecord);
+              whenOperational.add(OPERATIONAL);
+              if (i < info.getLeft().size() - 1) {
+                whenOperational.addAll(info.getLeft().subList(i + 1, info.getLeft().size()));
+              }
+              int n1 = calculateCombinations(Pair.of(whenOperational, info.getRight()), numbersPattern, cache);
+
+              List<Record> whenDamaged = new ArrayList<>(newRecord);
+              whenDamaged.add(Record.DAMAGED);
+              if (i < info.getLeft().size() - 1) {
+                whenDamaged.addAll(info.getLeft().subList(i + 1, info.getLeft().size()));
+              }
+              int n2 = calculateCombinations(Pair.of(whenDamaged, info.getRight()), numbersPattern, cache);
+
+              cache.put(cacheKey, n1 + n2);
+              return n1 + n2;
+            } else {
+              newRecord.add(info.getLeft().get(i));
+            }
+          }
+        } else {
+          return cache.get(cacheKey);
+        }
+      }
     }
-    return result;
+    return 0;
   }
 
   private Pattern buildPattern(List<Integer> numbers) {
     StringBuilder regex = new StringBuilder();
-    regex.append("(\\.)*");
+    regex.append("[\\.\\?]*");
     for (int i = 0; i < numbers.size(); i++) {
-      regex.append("(\\#){%d}".formatted(numbers.get(i)));
+      regex.append("([\\#\\?]{%d})".formatted(numbers.get(i)));
       if (i < numbers.size() - 1) {
-        regex.append("(\\.)+");
+        regex.append("[\\.\\?]+");
       }
     }
-    regex.append("(\\.)*");
+    regex.append("[\\.\\?]*");
     return Pattern.compile(regex.toString());
   }
 
   public static void main(String[] args) {
-    Day.run(Day12::new, "2023/D12_full.txt"); //, "2023/D12_full.txt");
+    Day.run(Day12::new, "2023/D12_small.txt"); //, "2023/D12_full.txt");
   }
 }
