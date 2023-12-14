1940b7c5872ab25cda0d45ca532cb66b4351582e
Manuel Fernández Molina <manuel.fernandez.molina@ing.com>
2023-12-14 11:35:51 +0100

    refactor: 2023 D14 refactored to Character[][] for performance reasons (~20secs -> 300ms)

--- src/main/java/es/ing/aoc/y2023/Day14.java
+++ src/main/java/es/ing/aoc/y2023/Day14.java
@@ -2,12 +2,12 @@ package es.ing.aoc.y2023;
 
 import es.ing.aoc.common.Day;
 import es.ing.aoc.common.MatrixUtils;
-import es.ing.aoc.common.Point;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
+
 
 public class Day14 extends Day {
 
@@ -17,53 +17,48 @@ public class Day14 extends Day {
   private static final PointFunction W = new PointFunction(x -> x, y -> y - 1);
 
   private static final int ITERATIONS = 1000000000;
-  private static final int MAX_SEQS_CACHE = 100;
+  private static final int CACHE_SIZE = 100;
   private static final int LENGTH_FOR_PATTERN_SEARCH = 10;
+  private static final char ROCK = 'O';
+  private static final char EMPTY = '.';
 
-  record PointFunction(Function<Integer, Integer> xFn, Function<Integer, Integer> yFn) {
-  }
-
-  record Map(List<Point> map, List<Point> rocks, List<Point> walls) {
-  }
+  record PointFunction(Function<Integer, Integer> xFn, Function<Integer, Integer> yFn) {}
 
   @Override
   protected String part1(String fileContents) throws Exception {
-    String[][] matrix = MatrixUtils.readMatrixFromFile(fileContents);
-    Map map = buildRocksAndWalls(matrix);
-    tiltRocks(matrix, map, N);
-    return String.valueOf(getNorthOverload(matrix, map.rocks));
+    Character[][] charsMatrix = MatrixUtils.readMatrixFromFile(fileContents, Character.class, s -> s.charAt(0));
+    tiltRocks(charsMatrix, N);
+    return String.valueOf(getNorthOverload(charsMatrix));
   }
 
   @Override
   protected String part2(String fileContents) throws Exception {
-    String[][] matrix = MatrixUtils.readMatrixFromFile(fileContents);
-    Map map = buildRocksAndWalls(matrix);
-
+    Character[][] charsMatrix = MatrixUtils.readMatrixFromFile(fileContents, Character.class, s -> s.charAt(0));
     List<Integer> loadSeqs = new ArrayList<>();
 
-    for (int i = 0; i < ITERATIONS; i++) {
-      tiltRocks(matrix, map, N);
-      tiltRocks(matrix, map, W);
-      tiltRocks(matrix, map, S);
-      tiltRocks(matrix, map, E);
+    for (int it = 0; it < ITERATIONS; it++) {
+      tiltRocks(charsMatrix, N);
+      tiltRocks(charsMatrix, W);
+      tiltRocks(charsMatrix, S);
+      tiltRocks(charsMatrix, E);
 
-      int overload = getNorthOverload(matrix, map.rocks);
+      int overload = getNorthOverload(charsMatrix);
 
       loadSeqs.add(overload);
-      if (loadSeqs.size() > MAX_SEQS_CACHE) {
+      if (loadSeqs.size() > CACHE_SIZE) {
         loadSeqs.remove(0);
       }
 
       int pattern = checkSequenceRepeatPattern(loadSeqs);
 
       if (pattern > 0) {
-        System.out.printf("Pattern detected: %d  %d\n", i, pattern);
-        while ((i + pattern) < ITERATIONS) {
-          i += pattern;
+        System.out.printf("Pattern detected: %d  %d\n", it, pattern);
+        while ((it + pattern) < ITERATIONS) {
+          it += pattern;
         }
       }
     }
-    return String.valueOf(getNorthOverload(matrix, map.rocks));
+    return String.valueOf(getNorthOverload(charsMatrix));
   }
 
   private int checkSequenceRepeatPattern(List<Integer> loadSeqs) {
@@ -80,48 +75,52 @@ public class Day14 extends Day {
     return 0;
   }
 
-  private Map buildRocksAndWalls(String[][] matrix) {
-    List<Point> rocks = new ArrayList<>(), walls = new ArrayList<>(), map = new ArrayList<>();
-    for (int x = 0; x < matrix.length; x++) {
-      for (int y = 0; y < matrix[x].length; y++) {
-        switch (matrix[x][y]) {
-          case "O" -> rocks.add(Point.of(x, y));
-          case "#" -> walls.add(Point.of(x, y));
+  private int getNorthOverload(Character[][] charsMatrix) {
+    int overload = 0;
+    for (int x = 0; x < charsMatrix.length; x++) {
+      for (int y = 0; y < charsMatrix[x].length; y++) {
+        if (ROCK==charsMatrix[x][y]) {
+          overload += charsMatrix.length - x;
         }
       }
     }
-    map.addAll(rocks);
-    map.addAll(walls);
-    return new Map(map, rocks, walls);
+    return overload;
   }
 
-  private int getNorthOverload(String[][] matrix, List<Point> rocks) {
-    return rocks.stream().mapToInt(p -> matrix.length - p.x).sum();
-  }
-
-  private void tiltRocks(String[][] matrix, Map map, PointFunction fn) {
+  private void tiltRocks(Character[][] matrix, PointFunction fn) {
     boolean movs;
+    int newX, newY, currX, currY;
     do {
       movs = false;
-      for (Point rock : map.rocks) {
-        boolean currentRockRoll;
-        do {
-          currentRockRoll = false;
-          Point newRockPoint = Point.of(fn.xFn().apply(rock.x), fn.yFn().apply(rock.y));
+      for (int x = 0; x < matrix.length; x++) {
+        for (int y = 0; y < matrix[x].length; y++) {
+          currX = x;
+          currY = y;
+          if (ROCK==matrix[x][y]) {
+            boolean currentRockRoll;
+            do {
+              currentRockRoll = false;
+              newX = fn.xFn().apply(currX);
+              newY = fn.yFn().apply(currY);
 
-          if (pointIsWithinLimits(matrix, newRockPoint) && !map.map.contains(newRockPoint)) {
-            movs = true;
-            currentRockRoll = true;
-            rock.x = newRockPoint.x;
-            rock.y = newRockPoint.y;
+              if (pointIsWithinLimits(matrix, newX, newY) && EMPTY==matrix[newX][newY]) {
+                movs = true;
+                currentRockRoll = true;
+                matrix[currX][currY] = EMPTY;
+                matrix[newX][newY] = ROCK;
+
+                currX = newX;
+                currY = newY;
+              }
+            } while (currentRockRoll);
           }
-        } while (currentRockRoll);
+        }
       }
     } while (movs);
   }
 
-  private boolean pointIsWithinLimits(String[][] matrix, Point p) {
-    return p.x >= 0 && p.x < matrix.length && p.y >= 0 && p.y < matrix[0].length;
+  private boolean pointIsWithinLimits(Character[][] matrix, int newX, int newY) {
+    return newX >= 0 && newX < matrix.length && newY >= 0 && newY < matrix[0].length;
   }
 
   public static void main(String[] args) {
