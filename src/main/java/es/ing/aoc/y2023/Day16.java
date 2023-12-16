bb9ee3410175c4c9ac223565826b490825776821
Manuel Fernández Molina <manuel.fernandez.molina@ing.com>
2023-12-16 10:01:16 +0100

    refactor: 2023 D16 solved (parallelStream)

--- src/main/java/es/ing/aoc/y2023/Day16.java
+++ src/main/java/es/ing/aoc/y2023/Day16.java
@@ -54,23 +54,28 @@ public class Day16 extends Day {
   @Override
   protected String part2(String fileContents) throws Exception {
     Character[][] charsMatrix = MatrixUtils.readMatrixFromFile(fileContents, Character.class, s -> s.charAt(0));
+    List<Beam> initialBeams = new ArrayList<>();
 
-    List<Integer> energies = new ArrayList<>();
-    for (int x=0; x<charsMatrix.length; x++){
-      for (int y=0; y<charsMatrix[x].length; y++){
-        if (x == 0){
-          energies.add(getEnergyFrom(charsMatrix, new Beam(Point.of(x, y), DOWN)));
-        } else if (y == 0){
-          energies.add(getEnergyFrom(charsMatrix, new Beam(Point.of(x, y), RIGHT)));
-        } else if (x == charsMatrix.length - 1){
-          energies.add(getEnergyFrom(charsMatrix, new Beam(Point.of(x, y), UP)));
-        } else if (y == charsMatrix[x].length - 1){
-          energies.add(getEnergyFrom(charsMatrix, new Beam(Point.of(x, y), LEFT)));
+    for (int x = 0; x < charsMatrix.length; x++) {
+      for (int y = 0; y < charsMatrix[x].length; y++) {
+        if (x==0) {
+          initialBeams.add(new Beam(Point.of(x, y), DOWN));
+        } else if (y==0) {
+          initialBeams.add(new Beam(Point.of(x, y), RIGHT));
+        } else if (x==charsMatrix.length - 1) {
+          initialBeams.add(new Beam(Point.of(x, y), UP));
+        } else if (y==charsMatrix[x].length - 1) {
+          initialBeams.add(new Beam(Point.of(x, y), LEFT));
         }
       }
     }
 
-    return String.valueOf(energies.stream().mapToInt(Integer::valueOf).max().orElse(0));
+    return String.valueOf(initialBeams
+        .parallelStream()
+        .map(b -> getEnergyFrom(charsMatrix, b))
+        .mapToInt(Integer::valueOf)
+        .max()
+        .orElse(0));
   }
 
   private int getEnergyFrom(Character[][] charsMatrix, Beam initialBeam) {
@@ -78,8 +83,6 @@ public class Day16 extends Day {
     List<Beam> alreadyVisited = new ArrayList<>();
     List<Beam> beams = new ArrayList<>();
     beams.add(initialBeam);
-
-    System.out.printf("[%-3d,%-3d] - %s\n", initialBeam.pos.x, initialBeam.pos.y, initialBeam.dir);
 
     while (!beams.isEmpty()) {
 
@@ -141,7 +144,6 @@ public class Day16 extends Day {
   private boolean pointIsWithinLimits(Character[][] matrix, Point newPoint) {
     return newPoint.x >= 0 && newPoint.x < matrix.length && newPoint.y >= 0 && newPoint.y < matrix[0].length;
   }
-
 
   public static void main(String[] args) {
     Day.run(Day16::new, "2023/D16_small.txt", "2023/D16_full.txt");
