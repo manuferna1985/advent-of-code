81f21fcb7d80a7151bc5324f331413e424a1bbac
Manuel Fernández Molina <manuel.fernandez.molina@ing.com>
2023-12-18 10:18:24 +0100

    refactor: 2023 D18 part1

--- src/main/java/es/ing/aoc/common/Point.java
+++ src/main/java/es/ing/aoc/common/Point.java
@@ -3,78 +3,86 @@ package es.ing.aoc.common;
 import java.util.Objects;
 
 public class Point {
-    public int x;
-    public int y;
-    public int z;
+  public int x;
+  public int y;
+  public int z;
 
-    public Point(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
+  public Point(int x, int y, int z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
 
-    public Point(String x, String y, String z) {
-        this(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(z));
-    }
+  public Point(String x, String y, String z) {
+    this(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(z));
+  }
 
-    public Point(int x, int y) {
-        this(x, y, 0);
-    }
+  public Point(int x, int y) {
+    this(x, y, 0);
+  }
 
-    public Point(String x, String y) {
-        this(Integer.parseInt(x), Integer.parseInt(y));
-    }
+  public Point(String x, String y) {
+    this(Integer.parseInt(x), Integer.parseInt(y));
+  }
 
-    public Point(Point other) {
-        this(other.x, other.y, other.z);
-    }
+  public Point(Point other) {
+    this(other.x, other.y, other.z);
+  }
 
-    public Point(String[] strPoint) {
-        this.x = Integer.parseInt(strPoint[0]);
-        this.y = Integer.parseInt(strPoint[1]);
-        if (strPoint.length > 2) {
-            this.z = Integer.parseInt(strPoint[2]);
-        }
+  public Point(String[] strPoint) {
+    this.x = Integer.parseInt(strPoint[0]);
+    this.y = Integer.parseInt(strPoint[1]);
+    if (strPoint.length > 2) {
+      this.z = Integer.parseInt(strPoint[2]);
     }
+  }
 
-    public static Point of(int x, int y, int z) {
-        return new Point(x, y, z);
-    }
+  public static Point of(int x, int y, int z) {
+    return new Point(x, y, z);
+  }
 
-    public static Point of(int x, int y) {
-        return new Point(x, y);
-    }
+  public static Point of(int x, int y) {
+    return new Point(x, y);
+  }
 
-    public static Point of(String[] coords){
-        return new Point(coords);
-    }
+  public static Point of(String[] coords) {
+    return new Point(coords);
+  }
 
-    public String toString() {
-        return String.format("[%d,%d,%d]", x, y, z);
-    }
+  public String toString() {
+    return String.format("[%d,%d,%d]", x, y, z);
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return x == point.x && y == point.y && z == point.z;
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this==o) return true;
+    if (o==null || getClass()!=o.getClass()) return false;
+    Point point = (Point) o;
+    return x==point.x && y==point.y && z==point.z;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y, z);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(x, y, z);
+  }
 
-    public int getX() {
-        return x;
-    }
+  public int getX() {
+    return x;
+  }
 
-    public int getY() {
-        return y;
-    }
+  public int getY() {
+    return y;
+  }
 
-    public int getZ() {
-        return z;
-    }
+  public int getZ() {
+    return z;
+  }
+
+  public static Point max(Point p1, Point p2) {
+    return Point.of(Math.max(p1.x, p2.x), Math.max(p1.y, p2.y));
+  }
+
+  public static Point min(Point p1, Point p2) {
+    return Point.of(Math.min(p1.x, p2.x), Math.min(p1.y, p2.y));
+  }
 }
--- src/main/java/es/ing/aoc/y2023/Day18.java
+++ src/main/java/es/ing/aoc/y2023/Day18.java
@@ -1,28 +1,116 @@
 package es.ing.aoc.y2023;
 
 import es.ing.aoc.common.Day;
+import es.ing.aoc.common.Point;
+import org.apache.commons.lang3.Range;
+
+import java.util.HexFormat;
+import java.util.function.BiFunction;
 
 public class Day18 extends Day {
 
+  record Segment(Point p1, Point p2) {
+  }
 
   @Override
   protected String part1(String fileContents) throws Exception {
-
-    String[] lines = fileContents.split(System.lineSeparator());
-
-
-    return String.valueOf("");
+    return String.valueOf(getDiggedPoints(fileContents, this::getNormalPoint));
   }
 
   @Override
   protected String part2(String fileContents) throws Exception {
-
-    String[] lines = fileContents.split(System.lineSeparator());
-
-
-    return String.valueOf("");
+    return String.valueOf(getDiggedPoints(fileContents, this::getNormalPoint));
   }
 
+  private int getDiggedPoints(String fileContents, BiFunction<String, Point, Point> fn) {
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    Point min = Point.of(Integer.MAX_VALUE, Integer.MAX_VALUE);
+    Point max = Point.of(Integer.MIN_VALUE, Integer.MIN_VALUE);
+    Point s1 = Point.of(0, 0);
+
+    Point[] edges = new Point[lines.length];
+    int i=0;
+    for (String line : lines) {
+      Point s2 = fn.apply(line, s1);
+      edges[i++] = s2;
+      max = Point.max(max, s2);
+      min = Point.min(min, s2);
+      s1 = s2;
+    }
+
+    int digged = 0;
+    for (int x = min.x; x <= max.x; x++) {
+      System.out.println(x);
+      for (int y = min.y; y <= max.y; y++) {
+        if (pointInPoligon(x, y, edges) || pointInsidePoligon(x, y, edges)) {
+          digged++;
+        }
+      }
+    }
+
+    return digged;
+  }
+
+  private Point getNormalPoint(String line, Point prev) {
+    String[] parts = line.split(" ");
+    int len = Integer.parseInt(parts[1]);
+
+    Point s2 = switch (parts[0].charAt(0)) {
+      case 'R' -> Point.of(prev.x, prev.y + len);
+      case 'D' -> Point.of(prev.x + len, prev.y);
+      case 'L' -> Point.of(prev.x, prev.y - len);
+      case 'U' -> Point.of(prev.x - len, prev.y);
+      default -> throw new IllegalStateException("Unexpected value: " + parts[0].charAt(0));
+    };
+    return s2;
+  }
+
+  private Point getHexPoint(String line, Point prev) {
+    String[] parts = line.split(" ");
+    int len = HexFormat.fromHexDigits(parts[2].substring(2, parts[2].length() - 2));
+
+    Point s2 = switch (parts[2].charAt(parts[2].length() - 2)) {
+      case '0' -> Point.of(prev.x, prev.y + len);
+      case '1' -> Point.of(prev.x + len, prev.y);
+      case '2' -> Point.of(prev.x, prev.y - len);
+      case '3' -> Point.of(prev.x - len, prev.y);
+      default -> throw new IllegalStateException("Unexpected value: " + parts[0].charAt(0));
+    };
+    return s2;
+  }
+
+  private boolean pointInPoligon(int x, int y, Point[] edges) {
+
+    for (int i = 0, j=1; i < edges.length; i++) {
+      if (edges[i].x == x && edges[j].x == x && Range.between(edges[i].y, edges[j].y).contains(y)){
+        return true;
+      }
+      if (edges[i].y == y && edges[j].y == y && Range.between(edges[i].x, edges[j].x).contains(x)){
+        return true;
+      }
+
+      j = (j + 1) % edges.length;
+    }
+    return false;
+  }
+
+  private boolean pointInsidePoligon(int x, int y, Point[] edges) {
+    int i = 0;
+    int j = edges.length - 1;
+    boolean ok = false;
+
+    for (i = 0; i < edges.length; i++) {
+      if ((edges[i].y < y && edges[j].y >= y) || (edges[j].y < y && edges[i].y >= y)) {
+        if (edges[i].x + (y - edges[i].y) / (edges[j].y - edges[i].y) * (edges[j].x - edges[i].x) < x) {
+          ok = !ok;
+        }
+      }
+      j = i;
+    }
+
+    return ok;
+  }
 
   public static void main(String[] args) {
     Day.run(Day18::new, "2023/D18_small.txt", "2023/D18_full.txt");
