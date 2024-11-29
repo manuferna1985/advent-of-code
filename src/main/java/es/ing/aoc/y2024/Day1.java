3233b9f760299fc3cdfde75ae67900837baed497
manuel.fernandez.molina <manuel.fernandez.molina@ing.com>
2024-11-29 11:32:58 +0100

    feat: structure for the Year 2024 challenge

---
+++ src/main/java/es/ing/aoc/y2024/Day1.java
@@ -0,0 +1,32 @@
+package es.ing.aoc.y2024;
+
+import es.ing.aoc.common.Day;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day1 extends Day {
+
+  @Override
+  protected String part1(String fileContents) throws Exception {
+
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    return "";
+  }
+
+  @Override
+  protected String part2(String fileContents) throws Exception {
+
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    return "";
+  }
+
+  public static void main(String[] args) {
+    Day.run(Day1::new, "2024/D1_small.txt", "2024/D1_full.txt");
+  }
+}
