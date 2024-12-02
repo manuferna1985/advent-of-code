af5b450cb536614ecb096f9cc71bd356575b2144
manuel.fernandez.molina <manuel.fernandez.molina@ing.com>
2024-12-02 08:06:42 +0100

    feat: day3 structure

---
+++ src/main/java/es/ing/aoc/y2024/Day3.java
@@ -0,0 +1,31 @@
+package es.ing.aoc.y2024;
+
+import es.ing.aoc.common.Day;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Day3 extends Day {
+
+  @Override
+  protected String part1(String fileContents) throws Exception {
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    return "";
+  }
+
+  @Override
+  protected String part2(String fileContents) throws Exception {
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    return "";
+  }
+
+  public static void main(String[] args) {
+    Day.run(Day3::new, "2024/D3_small.txt", "2024/D3_full.txt");
+  }
+}
