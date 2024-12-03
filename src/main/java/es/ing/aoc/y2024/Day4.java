30a52e19c66ad6c036a07dbbe3b1f69913b1b356
manuel.fernandez.molina <manuel.fernandez.molina@ing.com>
2024-12-03 10:18:21 +0100

    feat: day4 structure

---
+++ src/main/java/es/ing/aoc/y2024/Day4.java
@@ -0,0 +1,24 @@
+package es.ing.aoc.y2024;
+
+import es.ing.aoc.common.Day;
+
+public class Day4 extends Day {
+
+  @Override
+  protected String part1(String fileContents) throws Exception {
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    return "";
+  }
+
+  @Override
+  protected String part2(String fileContents) throws Exception {
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    return "";
+  }
+
+  public static void main(String[] args) {
+    Day.run(Day3::new, "2024/D4_small.txt", "2024/D4_full.txt");
+  }
+}
