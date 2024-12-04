fe363934033caba4b997f150b87cdc282ea9938b
manuel.fernandez.molina <manuel.fernandez.molina@ing.com>
2024-12-04 07:34:23 +0100

    feat: day5 structure

---
+++ src/main/java/es/ing/aoc/y2024/Day5.java
@@ -0,0 +1,24 @@
+package es.ing.aoc.y2024;
+
+import es.ing.aoc.common.Day;
+
+public class Day5 extends Day {
+
+  @Override
+  protected String part1(String fileContents) throws Exception {
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    return "";
+  }
+
+  @Override
+  protected String part2(String fileContents) throws Exception {
+    String[] lines = fileContents.split(System.lineSeparator());
+
+    return "";
+  }
+
+  public static void main(String[] args) {
+    Day.run(Day5::new, "2024/D5_small.txt", "2024/D5_full.txt");
+  }
+}
