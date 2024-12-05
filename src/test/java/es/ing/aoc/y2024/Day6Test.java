3eb9a4f3a1a4d0a5f1152854b10e0f0d5e69d597
manuel.fernandez.molina <manuel.fernandez.molina@ing.com>
2024-12-05 06:54:06 +0100

    feat: day6 structure

---
+++ src/test/java/es/ing/aoc/y2024/Day6Test.java
@@ -0,0 +1,24 @@
+package es.ing.aoc.y2024;
+
+import es.ing.aoc.common.Day;
+import es.ing.aoc.common.Pair;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day6Test {
+
+  @Test
+  void testSmallProblem() {
+    Pair<String, String> results = Day.run(Day6::new, "2024/D6_small.txt");
+    assertEquals("", results.a);
+    assertEquals("", results.b);
+  }
+
+  @Test
+  void testFullProblem() {
+    Pair<String, String> results = Day.run(Day6::new, "2024/D6_full.txt");
+    assertEquals("", results.a);
+    assertEquals("", results.b);
+  }
+}
